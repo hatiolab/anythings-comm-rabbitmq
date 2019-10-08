0c587c163cd5a38faf19aa383e5abdf45ccb1624
wryang <yang@yang-mac.local>
2019-10-08 14:19:21 +0900

    수신 메시지에 처리 완료 여부 추가
    1

--- src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
+++ src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
@@ -13,12 +13,14 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 	private final Message message;
 	private final String vHost;
 	public final String queueName;
+	private boolean isExecuted;
 	
 	public SystemMessageReceiveEvent(Object source, Message message, String vHost) {
 		super(source);
 	    this.message = message;
 	    this.vHost =vHost;
 	    this.queueName = message.getMessageProperties().getConsumerQueue();
+	    this.isExecuted = false;
 	}
 	
 	public Message getMessage() {
@@ -31,5 +33,13 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 	
 	public String getQueueName() {
 		return queueName;
+	}
+
+	public boolean isExecuted() {
+		return isExecuted;
+	}
+
+	public void setExecuted(boolean isExecuted) {
+		this.isExecuted = isExecuted;
 	}
 }
