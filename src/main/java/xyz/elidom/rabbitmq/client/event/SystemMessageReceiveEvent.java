48d67a1a408f9aea5be15b6dd5bd4a681638a115
wryang <yang@yang-mac.local>
2019-11-18 10:50:26 +0900

    queue 규칙에 vendor, equipType, cd 추가

--- src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
+++ src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
@@ -3,6 +3,11 @@ package xyz.elidom.rabbitmq.client.event;
 import org.springframework.amqp.core.Message;
 import org.springframework.context.ApplicationEvent;
 
+import xyz.elidom.rabbitmq.message.MessageProperties;
+import xyz.elidom.rabbitmq.message.ReceiveMessage;
+import xyz.elidom.util.FormatUtil;
+import xyz.elidom.util.ValueUtil;
+
 /**
  * system 메시지 event
  * @author yang
@@ -13,14 +18,40 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 	private final Message message;
 	private final String vHost;
 	public final String queueName;
+	
 	private boolean isExecuted;
+	
+	private final String equipType;
+	private final String equipCd;
+	private final String equipVendor;
 	
 	public SystemMessageReceiveEvent(Object source, Message message, String vHost) {
 		super(source);
 	    this.message = message;
 	    this.vHost =vHost;
 	    this.queueName = message.getMessageProperties().getConsumerQueue();
+	    
 	    this.isExecuted = false;
+	    
+	    String bodyString = new String(message.getBody());
+	    ReceiveMessage receiveMessage = FormatUtil.underScoreJsonToObject(bodyString, ReceiveMessage.class);
+		MessageProperties properties = receiveMessage.getProperties();	    
+		
+	    String[] splitQueue = properties.getSourceId().split("/");
+	    
+	    if(ValueUtil.isEmpty(splitQueue) == false && splitQueue.length > 3) {
+	    	this.equipType = splitQueue[3];
+	    	
+	    	if(splitQueue.length > 4 ) this.equipVendor = splitQueue[4];
+	    	else this.equipVendor = "";
+	    	
+	    	if(splitQueue.length > 5 ) this.equipCd = splitQueue[5];
+	    	else this.equipCd = "";
+	    } else {
+	    	this.equipType = "";
+	    	this.equipVendor = "";
+	    	this.equipCd = "";
+	    }
 	}
 	
 	public Message getMessage() {
@@ -41,5 +72,15 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 
 	public void setExecuted(boolean isExecuted) {
 		this.isExecuted = isExecuted;
+	}
+	
+	public String getEquipType() {
+		return equipType;
+	}
+	public String getEquipCd() {
+		return equipCd;
+	}
+	public String getEquipVendor() {
+		return equipVendor;
 	}
 }
