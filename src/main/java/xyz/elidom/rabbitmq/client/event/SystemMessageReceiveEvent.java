3cb39dc2368879174f8bcb733c2ddf7f2ed3dd1b
hatiolab <shortstop@hatiolab.com>
2019-11-19 20:55:53 +0900

    이벤트에 stageCd get/set 추가

--- src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
+++ src/main/java/xyz/elidom/rabbitmq/client/event/SystemMessageReceiveEvent.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationEvent;
 
 import xyz.elidom.rabbitmq.message.MessageProperties;
 import xyz.elidom.rabbitmq.message.ReceiveMessage;
+import xyz.elidom.sys.SysConstants;
 import xyz.elidom.util.FormatUtil;
 import xyz.elidom.util.ValueUtil;
 
@@ -21,36 +22,35 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 	
 	private boolean isExecuted;
 	
+	private final String stageCd;
 	private final String equipType;
 	private final String equipCd;
 	private final String equipVendor;
 	
 	public SystemMessageReceiveEvent(Object source, Message message, String vHost) {
 		super(source);
+		
 	    this.message = message;
-	    this.vHost =vHost;
+	    this.vHost = vHost;
 	    this.queueName = message.getMessageProperties().getConsumerQueue();
-	    
 	    this.isExecuted = false;
 	    
 	    String bodyString = new String(message.getBody());
 	    ReceiveMessage receiveMessage = FormatUtil.underScoreJsonToObject(bodyString, ReceiveMessage.class);
 		MessageProperties properties = receiveMessage.getProperties();	    
-		
-	    String[] splitQueue = properties.getSourceId().split("/");
+	    String[] splitQueue = properties.getSourceId().split(SysConstants.SLASH);
 	    
-	    if(ValueUtil.isEmpty(splitQueue) == false && splitQueue.length > 3) {
-	    	this.equipType = splitQueue[3];
+	    if(ValueUtil.isNotEmpty(splitQueue) && splitQueue.length > 2) {
+	    	this.stageCd = splitQueue[2];
+	    	this.equipType = (splitQueue.length > 3) ? splitQueue[3] : SysConstants.EMPTY_STRING;
+	    	this.equipVendor = (splitQueue.length > 4) ? splitQueue[4] : SysConstants.EMPTY_STRING;
+	    	this.equipCd = (splitQueue.length > 5) ? splitQueue[5] : SysConstants.EMPTY_STRING;
 	    	
-	    	if(splitQueue.length > 4 ) this.equipVendor = splitQueue[4];
-	    	else this.equipVendor = "";
-	    	
-	    	if(splitQueue.length > 5 ) this.equipCd = splitQueue[5];
-	    	else this.equipCd = "";
 	    } else {
-	    	this.equipType = "";
-	    	this.equipVendor = "";
-	    	this.equipCd = "";
+	    	this.stageCd = SysConstants.EMPTY_STRING;
+	    	this.equipType = SysConstants.EMPTY_STRING;
+	    	this.equipVendor = SysConstants.EMPTY_STRING;
+	    	this.equipCd = SysConstants.EMPTY_STRING;
 	    }
 	}
 	
@@ -74,12 +74,18 @@ public class SystemMessageReceiveEvent extends ApplicationEvent {
 		this.isExecuted = isExecuted;
 	}
 	
+	public String getStageCd() {
+		return stageCd;
+	}
+	
 	public String getEquipType() {
 		return equipType;
 	}
+	
 	public String getEquipCd() {
 		return equipCd;
 	}
+	
 	public String getEquipVendor() {
 		return equipVendor;
 	}
